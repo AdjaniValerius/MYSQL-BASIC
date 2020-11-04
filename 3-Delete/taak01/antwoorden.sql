e48614b8a6f013d648c9ebbdc052fced45421b2e
Adjani Valerius <2068687@talnet.nl>
2020-11-04 14:17:16 +0100

    mySQL 3- taak1

--- 3-Delete/taak01/antwoorden.sql
+++ 3-Delete/taak01/antwoorden.sql
@@ -1,9 +1,10 @@
 -- Opdracht 1 
-
+DELETE FROM player WHERE name = "David Sylva"
 -- Opdracht 2 
-
+DELETE FROM player WHERE club="Willem II"
 -- Opdracht 3
-
+DELETE FROM player WHERE nationality="Belgium" AND club="FC Barcelona"
 -- Opdracht 4
-
+DELETE FROM player WHERE age>=38
 -- Opdracht 5
+DELETE id FROM player WHERE id=167905 OR id=169595
