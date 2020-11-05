f5fbf4dfe7cd037df23327bea1c7339878daea2e
Adjani Valerius <2068687@talnet.nl>
2020-11-05 16:08:02 +0100

    mySQL 3- taak2

--- 3-Delete/taak02/antwoorden.sql
+++ 3-Delete/taak02/antwoorden.sql
@@ -1,5 +1,6 @@
 -- Opdracht 3
-
+ALTER TABLE `players` ADD PRIMARY KEY( `id`);
 -- Opdracht 4
-
+DELETE FROM players WHERE name="Rodrigo"
 -- Opdracht 5
+ALTER TABLE `players` ADD PRIMARY KEY( `name`);
